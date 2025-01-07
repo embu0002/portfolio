69b4439a355320a8301f5802270366ddc52af08d
embu0002 <embu0002@stud.kea.dk>
2025-01-07 12:18:50 +0100

    maybedone

--- java.js
+++ java.js
@@ -1,16 +1,17 @@
+/* Henvender til det html element, som har den specifikke class */
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
 
-burger.addEventListener("click", burgerClick);
+burger.addEventListener("click", burgerClick); /* Tilføjer eventlistener, så man kan klikke */
 function burgerClick() {
+  /* skifter klassen til active (bruges til at vise/skjule menu) */
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 }
-menu.addEventListener("click", menuClick);
+menu.addEventListener("click", menuClick); /* Tilføjer eventlistener, så man kan klikke */
 function menuClick() {
+  /* fjerne active */
   burger.classList.remove("active");
   nav.classList.remove("active");
 }
-
-/******************************************** Slide show *********************************************/
